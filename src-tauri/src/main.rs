69d49272b0b1d7b68b2aeb295bad282aaa6bfddc
Cleberton Kobay <Clebertonkobay@gmail.com>
2024-03-27 22:55:47 -0300

    função de manter a tela sempre no topo

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,7 +1,10 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use simulate::{self, Key, EventBuffer};
+use simulate::{self};
+// use tauri::Window;
+// use std::thread;
+// use std::time::Duration;
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -11,13 +14,17 @@ fn greet(name: &str) -> String {
 
 #[tauri::command]
 fn type_letter(letter: &str) {
-    simulate::move_mouse_relative(100, 100).unwrap();
     simulate::type_str(letter).unwrap();
+}
+
+#[tauri::command]
+fn move_mouse(x: i32, y: i32) {
+    simulate::move_mouse_relative(x, y).unwrap();
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet, type_letter,])
+        .invoke_handler(tauri::generate_handler![greet, type_letter, move_mouse,])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,26 @@
 import { useState } from "react";
 import { invoke } from "@tauri-apps/api/tauri";
+import { appWindow } from '@tauri-apps/api/window';
 import Keyboard from 'react-simple-keyboard';
 import 'react-simple-keyboard/build/css/index.css';
 import "./App.css";
 
+await appWindow.setAlwaysOnTop(true);
+
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+  const [words, setWords] = useState("");
 
-
-  async function onChange(letter: string) {
-    console.log("Input changed", letter);
-    await invoke('type_letter',{letter})
+  async function onChange(word: string) {
+    console.log("Input changed", word);
+    setWords(word)
   }
 
-  function onKeyPress(value: string) {
-    console.log("Button pressed", value);
+  async function onKeyPress(letter: string) {
+    console.log("Button pressed", letter);
+    window.blur()
+    await invoke('type_letter', { letter })
   }
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
-  }
 
   return (
     <div>
