6f7a763c464d9c670ba75c9fd9fcaeaa5a56fc4d
Matheus Bueno <matheusbartkev@gmail.com>
2024-06-11 23:59:15 -0300

    adicionando os headers

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,5 +1,11 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL:"",
+    baseURL:"http://192.168.3.42",
+    headers: {
+        'Access-Control-Allow-Headers': '*',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
+        'X-Requested-With': 'XMLHttpRequest'
+    }
 })
